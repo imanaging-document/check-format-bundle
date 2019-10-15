4f195b8414d1319ef68794c0955ae4af0086311e
n1n1du44 <a.auffray@imanaging-document.fr>
2019-10-15 22:52:55 +0200

    add static encodeUtf8

--- src/CheckFormatFile.php
+++ src/CheckFormatFile.php
@@ -165,7 +165,7 @@ class CheckFormatFile
    * @param $string
    * @return string
    */
-  private function encodeToUtf8($string) {
+  public static function encodeToUtf8($string) {
     return mb_convert_encoding($string, "UTF-8", mb_detect_encoding($string, "UTF-8, ISO-8859-1, ISO-8859-15", true));
   }
 }
